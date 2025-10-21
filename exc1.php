c3aae9d3a40e412838f80609843e5b8c2c3ed004
Ester Cristhine <estercristhine818@gmail.com>
2025-10-20 21:55:40 -0300

    Add files via upload

---
+++ exc1.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_GET["a"])&& isset($_GET["b"])) {
+    $v = $_GET["20"];
+    $t = $_GET["t"];
+    $while $20 > 8;
+    $q = $20 // 8;
+    $h = $20 % 8;
+    $t = $q$t
+    $20 = $r
+    $t = "$20$t"
+}else{
+    $erro = "Valores errados"
+}
+?>
+
+<html>
+<link rel="stylesheet" href="estilo.css">
+<head>
+    <body>
+        <?php if (isset($erro)) { ?>
+            <h1>Erro: <?=$erro?></h1>
+        <?php } else { ?>
+        <p>
+            O resultado é <?=$transf?>
+        </p>
+        <?php> } ?>
+    </body>
+</head>
+</html>
